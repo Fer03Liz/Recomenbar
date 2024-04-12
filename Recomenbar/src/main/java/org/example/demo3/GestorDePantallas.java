37a5b81ff0db86ad7a482b681e48057a6435c8b0
Fer03Liz <fjlizarazog@gmail.com>
2024-04-11 20:59:24 -0500

    Primer exito del gestor, home

--- Recomenbar/src/main/java/org/example/demo3/GestorDePantallas.java
+++ Recomenbar/src/main/java/org/example/demo3/GestorDePantallas.java
@@ -1,6 +1,43 @@
 package org.example.demo3;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GestorDePantallas {
-    //Manejar todas las pantallas
+    private final Map<String, String> rutasFXML;
+    private final Stage escenarioPrincipal;
+
+    public GestorDePantallas(Stage escenarioPrincipal) {
+        this.escenarioPrincipal = escenarioPrincipal;
+        this.rutasFXML = new HashMap<>();
+        this.rutasFXML.put("Home", "/org/example/demo3/Home.fxml");
+        this.rutasFXML.put("Login", "/org/example/demo3/Login.fxml");
+        this.rutasFXML.put("PostLogin", "/org/example/demo3/PostLogin.fxml");
+        this.rutasFXML.put("Register", "/org/example/demo3/Register.fxml");
+        this.rutasFXML.put("Reservar", "/org/example/demo3/Reservar.fxml");
+    }
+
+    public void mostrarPantalla(String rutaFXML) {
+        try {
+            String rutaCompletaFXML = rutasFXML.get(rutaFXML);
+            if (rutaCompletaFXML == null) {
+                System.err.println("No se encontró la ruta FXML para: " + rutaFXML);
+                return;
+            }
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(rutaCompletaFXML));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            escenarioPrincipal.setScene(scene);
+            escenarioPrincipal.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
--- Recomenbar/src/main/java/org/example/demo3/HelloApplication.java
+++ Recomenbar/src/main/java/org/example/demo3/HelloApplication.java
@@ -1,23 +1,18 @@
 package org.example.demo3;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-//Develop
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("HOME");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        // Instancia el gestor de pantallas y muestra la pantalla inicial
+        GestorDePantallas gestorDePantallas = new GestorDePantallas(primaryStage);
+        gestorDePantallas.mostrarPantalla("Home");
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
