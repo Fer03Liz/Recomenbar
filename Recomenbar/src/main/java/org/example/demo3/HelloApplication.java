33e6fa657e43730f8d00b0d5fddf1cd8847c5135
Andres Centanaro <centanaro@gmail.com>
2024-04-13 18:49:11 -0500

    Primera version de la bases de datos, imprime las tables en consola pero no en la interfaz gráfica

--- Recomenbar/src/main/java/org/example/demo3/HelloApplication.java
+++ Recomenbar/src/main/java/org/example/demo3/HelloApplication.java
@@ -3,6 +3,10 @@ package org.example.demo3;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.PrintStream;
+import java.sql.*;
+
+
 public class HelloApplication extends Application {
 
     @Override
@@ -11,8 +15,37 @@ public class HelloApplication extends Application {
         GestorDePantallas gestorDePantallas = new GestorDePantallas(primaryStage);
         gestorDePantallas.mostrarPantalla("Home");
     }
+    public static Connection ConectarBD(String bd ){//el string es para saber el nombre de la base de datos
+        // siempre y cuando esté dentro del mismo host y tenga los permisos (usuario y contraseña)
+        Connection conexion;
+        String host="jdbc:mysql://localhost/";
+        String user="root";//usuario de la base de datos (por defecto es root)
+        String pass= "Tinto56Ñ*52#";//contraseña de la base de datos
+        System.out.println("Conectando...");
+        try {
+            conexion= DriverManager.getConnection(host+bd,user,pass);
+            Statement statement=conexion.createStatement();
+            ResultSet resultSet=statement.executeQuery("SELECT * FROM CITY");
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("id")+" "+ "|"+resultSet.getString("name")+"|"+resultSet.getString("CountryCode")+"|"+resultSet.getString("District"));
+
+            }
+            conexion.close();
+            statement.close();
+            resultSet.close();
+            System.out.println("Coneccion exitosa!!");
+        }catch (SQLException e){
+            PrintStream printStream = System.err;
+            System.setErr(printStream);
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return conexion;
+    }
+
 
     public static void main(String[] args) {
+        Connection bd = ConectarBD("world");// nombre de la base de datos a la cual será conectada
         launch(args);
     }
 }
