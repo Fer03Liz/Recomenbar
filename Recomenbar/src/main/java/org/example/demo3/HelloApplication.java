a0dbd0a8247b586d0f8e6f03a686676d1bdc17be
Fer03Liz <fjlizarazog@gmail.com>
2024-04-15 20:06:29 -0500

    Pruebabase de datos 1

--- Recomenbar/src/main/java/org/example/demo3/HelloApplication.java
+++ Recomenbar/src/main/java/org/example/demo3/HelloApplication.java
@@ -1,9 +1,6 @@
 package org.example.demo3;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
@@ -20,20 +17,10 @@ public class HelloApplication extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        // Cargar el archivo FXML y obtener el controlador
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Home.fxml"));
-        Parent root = loader.load();
-        HelloController helloController = loader.getController();
-
-        // Instanciar el gestor de pantallas y establecerlo en el controlador
+    public void start(Stage primaryStage) {
+        // Instancia el gestor de pantallas y muestra la pantalla inicial
         GestorDePantallas gestorDePantallas = new GestorDePantallas(primaryStage);
-        helloController.setGestorDePantallas(gestorDePantallas);
-
-        // Configurar la escena y mostrarla
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        gestorDePantallas.mostrarPantalla("Home");
     }
 
     public static Connection conectarBD(String bd) throws SQLException {
