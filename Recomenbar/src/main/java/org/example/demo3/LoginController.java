24053d88e538767f820fe858bbc16f91fe6bab74
Fer03Liz <fjlizarazog@gmail.com>
2024-05-17 17:54:22 -0500

    Ya sirve validar login

--- Recomenbar/src/main/java/org/example/demo3/LoginController.java
+++ Recomenbar/src/main/java/org/example/demo3/LoginController.java
@@ -3,6 +3,7 @@ package org.example.demo3;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.example.demo3.Negocio.LogicaDelNegocio;
@@ -19,16 +20,27 @@ public class LoginController {
     private TextField ContraseñaField;
 
     @FXML
-    private void onLoginButtonClick(ActionEvent event) throws IOException {
+    private Label textAux;
+
+    public boolean validar(){
+        boolean valido = true;
         LogicaDelNegocio logicaDelNegocio = LogicaDelNegocio.getInstancia();
-        if (logicaDelNegocio.loginRealizado(CorreoField.getText(), ContraseñaField.getText())) {
+        if (!logicaDelNegocio.loginRealizado(CorreoField.getText(), ContraseñaField.getText())) {
+            valido= false;
+            textAux.setText("Correo o contraseña incorrectos");
+
+        }
+        return valido;
+    }
+
+    @FXML
+    private void onLoginButtonClick(ActionEvent event) throws IOException {
+        textAux.setText("");
+        if(validar()){
             Sesion sesion= Sesion.obtenerInstancia();
             sesion.setCorreo(CorreoField.getText());
             GestorDePantallas gestorDePantallas = GestorDePantallas.obtenerInstancia();
             gestorDePantallas.mostrarPantallaPostLogin(event);
-        } else {
-            ContraseñaField.setPromptText("Contraseña incorrecta");
-            CorreoField.setPromptText("Usuario incorrecta");
         }
     }
 
